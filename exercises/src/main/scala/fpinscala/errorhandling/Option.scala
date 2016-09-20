512a99ac45f378cc0043f4d4b86ae52f2aab5c01
Yun Ding <yding8@lsu.edu>
2016-09-20 15:17:33 -0500

    finish part of Option section
    	modified:   exercises/src/main/scala/fpinscala/errorhandling/Option.scala

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -3,19 +3,29 @@ package fpinscala.errorhandling
 
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
+import java.util.regex._
+
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse(None)
 
   def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if (f(a)) Some(a) else None)
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+
 
 object Option {
   def failingFn(i: Int): Int = {
@@ -38,9 +48,31 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] = {
+    val m = mean(xs)
+    val l = xs.flatMap(v => math.pow(v - m, 2))
+    mean(l)
+  }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+  def pattern(s: String): Option[Pattern] =
+    try {
+      Some(Pattern.compile(s))
+    } catch {
+      case e: PatternSyntaxException => None
+    }
+
+  def mkMatcher(pat: String): Option[String => Boolean] =
+    pattern(pat) map (p => (s: String) => p.matcher(s).matches)
+
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (Some(a), Some(b)) => Some(f(a, b))
+    case _ => None
+  }
+
+  def bothMatch_2(pat1: String, pat2: String, s: String): Option[Boolean] = sys.error("todo")
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
 
